95fde5212176a2136c60f058b7e236eb9b1e273f
S4mu3l-Silv4 <samuelssone02@gmail.com>
2025-06-06 10:47:48 -0300

    Teste

--- assets/js/script-home.js
+++ assets/js/script-home.js
@@ -14,3 +14,49 @@
 
         ultimoScrollTop = scrollTop <= 0 ? 0 : scrollTop
     })
+
+// Nav bar hambúrguer em telas mobile:
+
+    class navBarMobile {
+        constructor(menuMobile, listaNav, linksNav) {
+            this.menuMobile = document.querySelector(menuMobile)
+            this.listaNav = document.querySelector(listaNav)
+            this.linksNav = document.querySelectorAll(linksNav)
+            this.classeAtiva = "ativa"
+            this.toqueClick = this.toqueClick.bind(this)
+        }
+
+        linksAnimados() {
+            this.linksNav.forEach((link, index) => {
+                link.style.animation
+                    ? (link.style.animation = "")
+                    : (link.style.animation = `linkNavOpaco 0.3s ease forwards ${
+                        index / 7 + 0.3
+                    }s`)
+            })
+        }
+
+        toqueClick() {
+            this.listaNav.classList.toggle(this.classeAtiva)
+            this.menuMobile.classList.toggle(this.classeAtiva)
+            this.linksAnimados()
+        }
+
+        eventoClick() {
+            this.menuMobile.addEventListener("click", this.toqueClick)
+        }
+
+        inicializar() {
+            if (this.menuMobile) {
+                this.eventoClick()
+            }
+            return this
+        }
+    }
+
+    const navBarMobileFalsa = new navBarMobile(
+        ".menu-mobile",
+        ".lista-nav",
+        ".lista-nav li",
+    )
+    navBarMobileFalsa.inicializar()
